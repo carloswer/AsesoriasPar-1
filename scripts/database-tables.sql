086dcd0716697e1bbbc869eaa3696c1fad2460e4
Carlos Eduardo Noriega Cazarez <Carlos Eduardo Noriega Cazarez>
2017-03-15 02:05:13 -0600

    Realizacion de scripts de la base de datos
    Se agregaron los scripts de la base de datos debido a que se eliminaron
    los que ya se tenian hechos
    
    Los scripts, uno es de la creacion de todas las tablas y el segundo es
    de los valores por default de las tablas que se necesitan para su
    funcionamiento
    
    :wq

--- scripts/database-tables.sql
+++ scripts/database-tables.sql
@@ -1,4 +1,4 @@
-DROP DATABASE asesoriaspar;
+-- DROP DATABASE asesoriaspar;
 
 CREATE DATABASE asesoriaspar CHARACTER SET utf8 COLLATE utf8_general_ci;
 
@@ -15,12 +15,52 @@ CREATE TABLE carrera(
 CREATE TABLE materia(
 	IDmateria INT AUTO_INCREMENT PRIMARY KEY,
 	Nombre varchar(100) NOT NULL,
+	Semestre tinyint NOT NULL,
 
 	-- Foranea	
 	Carrera INT, FOREIGN KEY (Carrera) REFERENCES carrera(IDcarrera) ON UPDATE CASCADE
 );
 
 
-CREATE TABLE (
-
+CREATE TABLE usuario(
+	IDusuario INT AUTO_INCREMENT PRIMARY KEY,
+	Nombre varchar(50) NOT NULL,
+	IDitson varchar (15) NOT NULL,
+	Apellido varchar(50) NOT NULL,
+	Contrasenia varchar (50) NOT NULL,
+	Telefono varchar (30) NOT NULL,
+	Correo varchar (50) NOT NULL,
+	Facebook varchar (30) NOT NULL,
+	Avatar varchar (30),
+	Estado bit NOT NULL,
+	ReqValidarHorario bit NOT NULL,
+	FechaRegistro DATE NOT NULL,
+	Rol varchar(15) NOT NULL,
+	
+	-- llaves foraneas
+	Carrera int, FOREIGN KEY (Carrera) REFERENCES carrera(IDcarrera) ON UPDATE CASCADE
 );
+
+
+CREATE TABLE horario(
+	IDdia INT AUTO_INCREMENT PRIMARY KEY,
+	Nombre varchar(10) NOT NULL,
+	aprobado bit NOT NULL,
+	
+	-- Foranea	
+	Usuario INT, FOREIGN KEY (Usuario) REFERENCES usuario(IDusuario) ON UPDATE CASCADE
+);
+
+
+CREATE TABLE asesoria(
+	IDasesoria INT AUTO_INCREMENT PRIMARY KEY,
+	fecha DATE NOT NULL,
+	hora TIME NOT NULL,
+	validacion bit NOT NULL,
+	descripcion text,
+	
+	-- Foranea	
+	Usuario INT, FOREIGN KEY (Usuario) REFERENCES usuario(IDusuario) ON UPDATE CASCADE,
+	Materia INT, FOREIGN KEY (Materia) REFERENCES materia(IDmateria) ON UPDATE CASCADE
+);
+
--- scripts/default-values.sql
+++ scripts/default-values.sql
@@ -1,6 +1,42 @@
-INSERT INTO materia(nombre) VALUES
-('Progra'),
-('Progra 2'),
-('SO'),
-('APDS'),
-('Diseño');
+INSERT INTO materia(nombre,semestre) VALUES
+('Progra',1),
+('Progra 2',2),
+('SO',2),
+('APDS',3),
+('Diseño',4);
+
+
+INSERT INTO carrera(nombre) VALUES 
+('ISW'),
+('Electronica'),
+('Mecatronica'),
+('Diseño grafico');
+
+
+INSERT INTO horario(nombre,aprobado) VALUES
+('lunes',1),
+('Martes',1),
+('Miercoles',1),
+('Jueves',1),
+('Viernes',1);
+
+
+INSERT INTO rol(nombreRol) VALUES
+('Administrador'),
+('Asesor'),
+('Asesorado');
+
+
+INSERT INTO usuario(Nombre,Apellido,IDitson,Contrasenia,Telefono,Correo,Facebook,avatar,Estado,ReqValidarHorario,FechaRegistro,Rol) VALUES 
+-- (nombre,apellido,IDitson,contrasenia,telefono,Correo,Facebook,avatar,Estado,ReqValidarHorario,FechaRegistro,rol)
+('root','root1','1','contraseniaroot','235123','root1.root@hotmail.com','facebookroot','',1,0,'2017-01-01',1),
+('Carlos','Zuniga','2134421','asdff','541234','zuniga1@hotmail.com','zuniga1','',1,0,'2017-01-22',1),
+('Carlos','Noriega','126079','vasfd','2345352','carlos1@hotmail.com','carlosnoriega','',1,0,'2017-01-26',1);
+
+INSERT INTO asesoria(fecha,hora,validacion,descripcion) VALUES
+('2017-01-29','03:43:43',1,'nada'),
+('2017-01-25','05:30:00',1,'Progrmacion 2, arreglos'),
+('2017-01-21','01:21:46',1,'nada');
+
+
+
